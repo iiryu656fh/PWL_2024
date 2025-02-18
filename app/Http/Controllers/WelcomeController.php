efd60554c25488b31670695e2b386767c7174e96
iiryu656fh <khoirotunnisa2507@gmail.com>
2025-02-18 13:05:42 +0700

    jobsheet 02_Praktikum 3_langkah 11

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -11,6 +11,8 @@ class WelcomeController extends Controller
     }
 
     public function greeting() {
-        return view('blog.hello', ['name' => 'Khoirotun Nisa']);
+        return view('blog.hello')
+            ->with('name', 'Khoirotun Nisa')
+            ->with('occupation', 'Astronaut');
     }
 }
