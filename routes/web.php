3f6da40d623058e5647e607872030f7862a64fd3
iiryu656fh <khoirotunnisa2507@gmail.com>
2025-02-18 12:06:29 +0700

    jobsheet 02_Praktikum 2_langkah 6 : modifikasi

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,13 +27,13 @@ Route::get('/world', function () {
     return 'World';
 });
 
-Route::get('/', function () {
-   return 'Selamat Datang'; 
-});
+// Route::get('/', function () {
+//    return 'Selamat Datang'; 
+// });
 
-Route::get('/about', function () {
-   return 'Khoirotun Nisa / 2341720057' ; 
-});
+// Route::get('/about', function () {
+//    return 'Khoirotun Nisa / 2341720057' ; 
+// });
 
 Route::get('/user/{name}', function ($name) {
     return 'Nama Saya '.$name;
@@ -42,9 +43,9 @@ Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Pos ke-: ' . $postId . ' Komentar ke-: ' . $commentId;
 });
 
-Route::get('/articles/{id}', function ($id) {
-    return 'Halaman Artikel dengan ID' . $id;
-});
+// Route::get('/articles/{id}', function ($id) {
+//     return 'Halaman Artikel dengan ID' . $id;
+// });
 
 Route::get('/user/{name?}', function ($name=null) {
     return 'Nama saya ' . $name;
@@ -58,3 +59,9 @@ Route::get('/user/{name?}', function ($name='John') {
 // praktikum 2
 
 Route::get('/hello', [WelcomeController::class, 'hello']);
+
+Route::get('/', [PageController::class, 'index']);
+
+Route::get('/about', [PageController::class, 'about']);
+
+Route::get('/articles/{id}', [PageController::class, 'articles']);
